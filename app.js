ebd74dadc7371fe86044592994ed868af4c90427
Balaji-Sabbineni <balajisabbineni@gmail.com>
2024-08-09 13:22:55 +0530

    add rate limit

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ const UserRouter = require('./routes/user.route');
 
 const app = express();
 
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // max 100 requests per windowMs
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
+
+app.get('/:path', function(req, res) {
+  let path = req.params.path;
+  if (isValidPath(path))
+    res.sendFile(path);
+});
+
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({extended: true}));
 
