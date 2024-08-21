7a62aa8139f77c5c7b65f8ba986d82609adb2325
Balaji-Sabbineni <balajisabbineni@gmail.com>
2024-08-21 12:08:00 +0530

    update envs

--- config/database.config.js
+++ config/database.config.js
@@ -5,12 +5,12 @@ const { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sen
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.AUTHDOMAIN,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APPID,
-  measurementId: process.env.MEASUREMENTID
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
 
@@ -20,8 +20,7 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Auth and Firestore
 const auth = getAuth(app);
-//const db = getFirestore(app);
-const connectionString = `mongodb+srv://${process.env.dbUser}:${process.env.dbPassword}@cluster0.7zfjhjz.mongodb.net/${process.env.dbName}?retryWrites=true&w=majority`;
+const connectionString = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7zfjhjz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const database = mongoose.connect(connectionString)
   .then(() => console.log("Mongodb connected"))
